058bc1ec049380b5d8309e48ab83ec329ce5fc93
Qin Guan <qinguan20040914@gmail.com>
2024-10-22 08:08:42 +0000

    Rectangle

---
+++ RectangleArea/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var points = Console.ReadLine().Split(" ").Select(p => double.Parse(p)).ToList();
+
+var xDist = Math.Abs(points[0] - points[2]);
+var yDist = Math.Abs(points[1] - points[3]);
+var area = xDist * yDist;
+
+Console.WriteLine(Math.Round(area, 3));
